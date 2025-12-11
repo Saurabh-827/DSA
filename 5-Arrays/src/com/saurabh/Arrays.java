73eab4df01a5869e6966b439c0700166757c198a
Saurabh-827 <saurabhrabta@gmail.com>
2025-12-11 23:56:09 +0530

    Working on the array data structures

---
+++ 5-Arrays/src/com/saurabh/Arrays.java
@@ -0,0 +1,17 @@
+public class Arrays {
+    public static void main(String[] args) {
+        //syntax of the array
+
+//        //datatype[] variable_name = new datatype[size];
+//        int[]  rollNum = new int[15];
+//        //or directly
+//        int[] rollNum2 = {1, 2, 3, 4 };
+
+        int[] rolNum;  //DECLARATION: of array,  rolNum getting defined in the STACK
+
+        rolNum = new int[5]; //INITIALISATION: here object is getting created in the HEAP
+
+
+
+    }
+}
